e6541d9b2bb0e52889276ffd9fe634a5e092db63
Yugal810 <24sonparatey@rbunagpur.in>
2026-03-19 15:39:55 +0530

    changed app/main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # <--- Add this import
 from .database import engine
 from . import models
-from .routes import users, files,folders,sharing,search
+from .routes import users, files, folders, sharing, search
 
 app = FastAPI(title="Distributed File Storage API")
 
+# 1. Define who is allowed to talk to your backend
+origins = [
+    "http://localhost:5173",  # Your React local dev server
+    "http://127.0.0.1:5173",
+    "https://distributed-file-storage-3g5k.onrender.com", # Your production URL
+]
+
+# 2. Add the Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Allows GET, POST, DELETE, etc.
+    allow_headers=["*"], # Allows Authorization headers
+)
+
 # Create tables
+
 models.Base.metadata.create_all(bind=engine)
 
 # Register routers
